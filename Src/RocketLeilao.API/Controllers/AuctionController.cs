007f740d3dd579b96e374efa73984bbb4ebb44b1
Julian Reina <julianreina@gmail.com>
2024-02-06 22:06:47 -0300

    Adicionar arquivos de projeto.

---
+++ Src/RocketLeilao.API/Controllers/AuctionController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RocketLeilao.API.UsesCases.Auctions.GetCurrent;
+
+namespace RocketLeilao.API.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class AuctionController : ControllerBase
+	{
+		[HttpGet]
+		public IActionResult GetCurrentAuction()
+		{
+			var useCase = new GetCurrentAuctionUseCase();
+			var result = useCase.Execute();
+			return Ok(result);
+		}
+	}
+}
---
+++ Src/RocketLeilao.API/UsesCases/Auctions/GetCurrent/GetCurrentAuctionUseCase.cs
@@ -0,0 +1,19 @@
+﻿using RocketLeilao.API.Entities;
+
+namespace RocketLeilao.API.UsesCases.Auctions.GetCurrent
+{
+	public class GetCurrentAuctionUseCase
+	{
+		public Auction Execute()
+		{
+			return new Auction
+			{
+				Id = 1,
+				Ends = DateTime.Now,
+				Starts = DateTime.Now,
+				Name = "Alo Romario"
+
+			};
+		}
+	}
+}
